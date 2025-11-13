b4c8956633054df9d8ece0347e01cb04bc31658b
Abishek <92878762+abishekgvincent@users.noreply.github.com>
2025-11-13 22:28:50 +0530

    Initialize Firebase and Firestore in firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore} from 'firebase/firestore'
+
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ''
+};
+
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const firestore = getFirestore(app);
+
+export { app, firestore};
